a8a8aac27a862763da7f2b3d44a2fed628cfd6b2
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-06-30 15:34:09 +0900

    state 함수로 사용하는법 commit

--- react-hooks/src/components/Example3.jsx
+++ react-hooks/src/components/Example3.jsx
@@ -16,7 +16,17 @@ function Example3() {
   );
 
   function click(){
-    setState({count : state.count + 1});
+    // setState({count : state.count + 1});
+    // 위의 코드는 객체로 들어옴
+
+    // 기존 state 값에 의존적으로 변경하고 싶다면, 객체가 아니라 함수로 사용
+    // 새로운 state를 받아서 새로운 state를 return 
+    setState((state) => {
+      return {
+        count : state.count + 1,
+      };
+      // 나중에 setState가 어떤 것을 의존해서 사용하고 있는지 확인할 수 있다.
+    })
   }
 
 }
