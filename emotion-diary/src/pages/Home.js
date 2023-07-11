6f67260c2f3f60a521ea7b999fb09d4d4fe51e84
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-07-11 23:28:57 +0900

    Add - Diary list component

--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -1,11 +1,28 @@
 import MyButton from 'components/MyButton';
 import MyHeader from 'components/MyHeader';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { DiaryStateContext } from 'App';
+import DiaryList from 'components/DiaryList';
 
 const Home = () => {
-    const [curDate, setCurDate] = useState(new Date());
+    const diaryList = useContext(DiaryStateContext);
 
+    const [data, setData] = useState([]);
+    const [curDate, setCurDate] = useState(new Date());
     const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+    useEffect(() => {
+        if (diaryList.length >= 1) {
+            const firstDay = new Date(curDate.getFullYear(), curDate.getMonth(), 1).getTime();
+            const lastDay = new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0).getTime();
+
+            setData(diaryList.filter(it => firstDay <= it.date && it.date <= lastDay));
+        }
+    }, [diaryList, curDate]);
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
 
     // increase month
     const increaseMonth = () => {
@@ -25,6 +42,7 @@ const Home = () => {
                 leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
                 rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
             />
+            <DiaryList diaryList={data} />
         </div>
     );
 };
